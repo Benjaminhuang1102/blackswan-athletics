433afa7d80e308ab6e2b104864f7df8e01f12bee
Benjaminhuang1102 <Benjaminhuang1102@gmail.com>
2025-07-02 21:21:42 -0400

    Update images.js

--- tap/images.js
+++ tap/images.js
@@ -1,1 +1,8 @@
-
+const backgroundImages = [
+  "https://images.unsplash.com/photo-1506744038136-46273834b3fb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&h=1200&w=800",
+  "https://images.unsplash.com/photo-1517649763962-0c623066013b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&h=1200&w=800",
+  "https://images.unsplash.com/photo-1526403224990-6d08a5c0129f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&h=1200&w=800",
+  "https://images.unsplash.com/photo-1486308510493-cb130fbf85f5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&h=1200&w=800",
+  "https://images.unsplash.com/photo-1494526585095-c41746248156?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&h=1200&w=800",
+  "https://images.unsplash.com/photo-1496317556649-f930d733eea0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&h=1200&w=800"
+];
